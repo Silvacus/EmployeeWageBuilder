ffaaee399e5367d739849964703557c7609c4f29
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-09-26 18:14:30 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,19 +3,9 @@ package employee;
 public class EmployeeWageBuilder {
 	
 	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-		int hrs=0;
-		int wage=0;
-		int monWage=0;
-		int day=1;
-		while(day <=20 && hrs<=100){
-		int attend =(int)Math.floor(Math.random()*10)%3;
-		switch(attend){
-		case 1: hrs=8;break;
-		case 2: hrs=4;break;
-		default: hrs=0;
-		}
-		wage=hrs*20;
-		monWage=monWage+wage;
-		day++;hrs+=hrs;}
-		System.out.println("Monthly Emp Wage: "+monWage);}}
+		
+	        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+                int attend =(int)Math.floor(Math.random()*10)%2;
+                if(attend==0)System.out.println("Emp is present");
+                else System.out.println("Emp is absent");
+	}}
