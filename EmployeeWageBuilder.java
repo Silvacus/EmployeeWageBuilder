131d4751cd1b11b798959a2161d9c997aa2bbc0e
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-09-26 18:11:42 +0530

    Add files via upload

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,21 @@
+package employee;
+
+public class EmployeeWageBuilder {
+	
+	public static void main(String[] args){
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		int hrs=0;
+		int wage=0;
+		int monWage=0;
+		int day=1;
+		while(day <=20 && hrs<=100){
+		int attend =(int)Math.floor(Math.random()*10)%3;
+		switch(attend){
+		case 1: hrs=8;break;
+		case 2: hrs=4;break;
+		default: hrs=0;
+		}
+		wage=hrs*20;
+		monWage=monWage+wage;
+		day++;hrs+=hrs;}
+		System.out.println("Monthly Emp Wage: "+monWage);}}
