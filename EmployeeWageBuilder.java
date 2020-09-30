4217c46f3f06429f8c3c73d11714fa32ac1d0b33
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-09-30 16:14:48 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -11,12 +11,15 @@ int day=1;
 while(day <=20 && hrs<=100){
 int attend =(int)Math.floor(Math.random()*10)%3;
 switch(attend){
-case 1: hrs=8;break;
-case 2: hrs=4;break;
+case 1: hrs=8;
+	break;
+case 2: hrs=4;
+	break;
 default: hrs=0;
 }
 wage=hrs*20;
 monWage=monWage+wage;
-day++;hrs+=hrs;}
+day++;
+hrs+=hrs;}
 System.out.println("Monthly Emp Wage: "+monWage);}}
 
