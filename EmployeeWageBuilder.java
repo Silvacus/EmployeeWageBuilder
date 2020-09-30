3dcec1a7c6f26df4488ea54b7867ec21ca2861b5
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-09-30 16:13:40 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -8,8 +8,10 @@ int hrs=0;
 int wage=0;
 int attend =(int)Math.floor(Math.random()*10)%3;
 switch(attend){
-case 1: hrs=8;break;
-case 2: hrs=4;break;
+case 1: hrs=8;
+        break;
+case 2: hrs=4;
+	break;
 default: hrs=0;
 }
 wage=hrs*20;
