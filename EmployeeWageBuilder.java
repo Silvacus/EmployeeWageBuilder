230cac971eb0bfc98d3850069b728e4882674b1d
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-09-30 16:00:47 +0530

    Update EmployeeWageBuilder.java

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -5,7 +5,9 @@ public class EmployeeWageBuilder {
 	public static void main(String[] args){
 System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 int attend =(int)Math.floor(Math.random()*10)%2;
-if(attend==0)System.out.println("Emp is present");
-else System.out.println("Emp is absent");	
-	        
-	}}
+if(attend==0)
+	System.out.println("Emp is present");
+else 
+	System.out.println("Emp is absent");	
+	}
+}
